7474283116a5ed05125a13115f1b1ed2c517acca
Trevor Winsett <trevorwin@live.com>
2016-04-26 11:30:53 -0600

    Create header.php

---
+++ resources/includes/header.php
@@ -0,0 +1,10 @@
+<?php
+    require("/resources/scripts/php/general.php"); // You can change the file location
+?>
+<!DOCTYPE html>
+  <html>
+    <head>
+        <link type="text/css" rel="stylesheet" src="resources/style/main.css" /> <!-- You can change the file location. -->
+    </head>
+    <body> <!-- Begin body here and end in footer. Put header in here and anything else you'd want on every page. -->
+    
