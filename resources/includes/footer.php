1c976da201fb520efd1c317af30c1948c4388883
Trevor Winsett <trevorwin@live.com>
2016-04-26 11:32:55 -0600

    Create footer.php

---
+++ resources/includes/footer.php
@@ -0,0 +1,9 @@
+
+  <!--
+    If you created any previous divs and didn't end them in header, you can end them here.
+    You can also begin you're bottom footer before you end body tag.
+  -->
+ 
+  </body>
+  <title><?php echo $page; ?> | <?php echo $sitename; ?></title>
+</html>
